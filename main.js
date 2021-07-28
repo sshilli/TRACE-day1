9ccbe5ed2cdc19e8ee67ff78b164ed35e25656b6
Sydney <sshilli@clemson.edu>
2021-07-27 21:06:05 -0400

    Feat: syntax problem fixed

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function result() {
     newStory = newStory.replace(':insertx:', xItem);
     newStory = newStory.replace(':insertx:', xItem);
     newStory = newStory.replace(':inserty:', yItem);
-    newStory = newStory.replace(':insertZ:', zItem);
+    newStory = newStory.replace(':insertz:', zItem);
   
   
     if(customName.value !== '') {
